7f17abf49bbba40a2efbbbd8c447321bb3ef7e9e
Abhishek Basu <abhishekbasu1993@gmail.com>
2021-11-25 19:20:17 +0530

    final commit

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    if (req.url == "/") {
-        res.end(`Welcome to Home Page`);
-    } else if (req.url == "/about") {
-        res.end(`This is our About Page`);
-    } else {
-        res.end(`
-    <h1>Oops</h1>
-    <p>We can't seem to find what you are looking for</p>
-    <a href="/">Back to Home</a>
-    `);
-    }
+const EventEmitter = require("events");
+const event = new EventEmitter();
+
+event.on("response", (name, age) => {
+    console.log(`Data received: ${name} is ${age} years old`);
 });
 
-server.listen(5002, () => console.log("Server running!!!"));
+event.emit("response", "John", 34);
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+    console.log("response generated");
+    res.end("Welcome");
+});
+
+server.listen(5001);
